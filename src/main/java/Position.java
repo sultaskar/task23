3ee4b0c51d7aeb86f0a19f85c6de02df84f56375
sultaskar <66567868+sultaskar@users.noreply.github.com>
2020-08-13 13:17:44 +0500

    Update Position.java

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,8 +1,8 @@
-public class Position {
+public class Good {
     String product;
     int quantity;
 
-    public Position(String product, int quantity) {
+    public Good(String product, int quantity) {
         this.product = product;
         this.quantity = quantity;
     }
