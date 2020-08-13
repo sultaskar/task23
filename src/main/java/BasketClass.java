52d334c5eeaf9a677b6915596d89c45a9fa55d46
sultaskar <66567868+sultaskar@users.noreply.github.com>
2020-08-13 13:21:49 +0500

    Second commit

--- src/main/java/BasketClass.java
+++ src/main/java/BasketClass.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class BasketClass implements Basket {
 
-    List<Position> goods = new LinkedList<Position>();
+    List<Good> goods = new LinkedList<Position>();
 
 
     public void addProduct(String product, int quantity) {
@@ -16,10 +16,10 @@ public class BasketClass implements Basket {
     }
 
     public void removeProduct(String product) {
-        Iterator<Position> it = goods.iterator();
+        Iterator<Good> it = goods.iterator();
         while (it.hasNext()) {
-            Position p = it.next();
-            if (product.equals(p.product)){
+            Good g = it.next();
+            if (product.equals(g.product)){
                 it.remove();
             }
         }
@@ -27,15 +27,15 @@ public class BasketClass implements Basket {
 
     public void updateProductQuantity(String product, int quantity) {
         int index = 0;
-        Iterator<Position> it = goods.iterator();
+        Iterator<Good> it = goods.iterator();
         while (it.hasNext()){
-            Position p = it.next();
-            if (product.equals(p.product)){
+            Good g = it.next();
+            if (product.equals(g.product)){
                 index = goods.indexOf(p);
                 it.remove();
             }
         }
-        goods.add(index, new Position(product, quantity));
+        goods.add(index, new Good(product, quantity));
     }
 
     public void clear() {
@@ -45,14 +45,14 @@ public class BasketClass implements Basket {
 
     public List<String> getProducts() {
         List<String> list = new ArrayList<String>();
-        for (Position g: goods)
+        for (Good g: goods)
             list.add(g.toString() + "\n");
         return list;
     }
 
     public int getProductQuantity(String product) {
         int quantity = 0;
-        for (Position g : goods)
+        for (Good g : goods)
             if (product.equals(g.product)) {
                 quantity = g.quantity;
             }
